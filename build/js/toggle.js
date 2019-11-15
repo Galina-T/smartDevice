c4df56f9dcc9f7e6f99c7bfa9d915b92e7b83b56
Galina-T <kazarinagg@gmail.com>
2019-11-15 12:21:03 +0300

    Добавляет обнавленный build

--- build/js/toggle.js
+++ build/js/toggle.js
@@ -1,50 +1,5 @@
 'use strict';
 
-// var HIDDEN_CLASS = 'hidden';
-// var TOGGLE_HIDDEN_CLASS = 'page-footer__toggle--close';
-// var TOGGLE_SHOW_CLASS = 'page-footer__toggle--open';
-// var TEXT_OPEN = 'Развернуть';
-// var TEXT_CLOSE = 'Свернуть';
-
-// var footerMenuToggle = document.querySelector('.page-footer__toggle');
-// var footerNavList = document.querySelector('.footer-nav__list');
-// var footerContactsList = document.querySelector('.footer-contacts__list');
-
-// function closeItem(el) {
-//   el.currentTarget.classList.add(HIDDEN_CLASS);
-//   el.target.innerText = TEXT_OPEN;
-//   el.target.classList.remove(TOGGLE_SHOW_CLASS);
-//   el.target.classList.add(TOGGLE_HIDDEN_CLASS);
-// }
-
-// function openItem(el) {
-//   el.currentTarget.classList.remove(HIDDEN_CLASS);
-//   el.target.innerText = TEXT_CLOSE;
-//   el.target.classList.remove(TOGGLE_HIDDEN_CLASS);
-//   el.target.classList.add(TOGGLE_SHOW_CLASS);
-// }
-
-
-// function closeItemClickHandler(evt) {
-//   evt.preventDefault();
-
-//   if (evt.target.classList.contains(footerMenuToggle)) {
-
-//     if (evt.target.innerText === TEXT_OPEN) {
-//       openItem(evt);
-//     } else {
-//       closeItem(evt);
-//     }
-//   }
-// }
-
-// function addHadlers() {
-//   footerNavList.addEventListener('click', closeItemClickHandler);
-//   footerContactsList.addEventListener('click', closeItemClickHandler);
-// }
-
-// addHadlers();
-
 var HIDDEN_CLASS = 'hidden';
 var TOGGLE_HIDDEN_CLASS = 'page-footer__toggle--close';
 var TOGGLE_SHOW_CLASS = 'page-footer__toggle--open';
